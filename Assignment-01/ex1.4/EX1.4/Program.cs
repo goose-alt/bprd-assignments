85eb6eb02c846c8c89abc91a4bae36d046d7548a
Andreas Nicolaj Tietgen <andreotietgen@hotmail.com>
2022-09-01 17:00:37 +0200

    Add simplify example from exercise

--- Assignment-01/ex1.4/EX1.4/Program.cs
+++ Assignment-01/ex1.4/EX1.4/Program.cs
@@ -5,7 +5,12 @@ Expr expr1 = new Add(new CstI(17), new Var("z"));
 Expr expr2 = new Mul(new Var("y"), new CstI(2));
 Expr expr3 = new Sub(expr1, expr2);
 
+Expr simplifyExample = new Mul(new Add(new CstI(1), new CstI(0)), new Add(new Var("x"), new CstI(0)));
+
 System.Console.WriteLine(expr);
 System.Console.WriteLine(expr1);
 System.Console.WriteLine(expr2);
 System.Console.WriteLine(expr3);
+
+System.Console.WriteLine($"Before Simplification: {simplifyExample}");
+System.Console.WriteLine($"After Simplification: {simplifyExample.Simplify()}");
