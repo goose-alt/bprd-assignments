6496dbbb5c60b9333456c83eb3c637d81ff87641
amalieboegild <abso@itu.dk>
2022-09-22 19:02:46 +0200

    Solve second part of 4.2

--- Assignment-04/PLC-ex4.2.fs
+++ Assignment-04/PLC-ex4.2.fs
@@ -12,5 +12,17 @@ val e1: Absyn.expr =
 val it: int = 500500
 
 //2
+> let e2 = fromString "let power n = if 0<n then 3 * power (n-1) else 1 in power 8 end";;
+val e2: Absyn.expr =
+  Letfun
+    ("power", "n",
+     If
+       (Prim ("<", CstI 0, Var "n"),
+        Prim ("*", CstI 3, Call (Var "power", Prim ("-", Var "n", CstI 1))),
+        CstI 1), Call (Var "power", CstI 8))
+
+> run e2;;
+val it: int = 6561
+
 //3
 //4
