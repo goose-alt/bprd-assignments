f46ce4b3d279f496262ebfdc499215d859fc1fd1
amalieboegild <abso@itu.dk>
2022-09-24 20:18:36 +0200

    Done with 4.2

--- Assignment-04/PLC-ex4.2/compileAndRun.sh
+++ Assignment-04/PLC-ex4.2/compileAndRun.sh
@@ -3,4 +3,4 @@ CURRENT_DIR="$( cd "$( dirname "$0" )" && pwd )"
 BIN_DIR=$CURRENT_DIR/../bin
 
 mono $BIN_DIR/fslex.exe --unicode FunLex.fsl \
-    && dotnet fsi -r $BIN_DIR/FsLexYacc.Runtime.dll Absyn.fs FunPar.fs FunLex.fs Parse.fs Fun.fs ParseAndRun.fs
+    && fsharpi -r $BIN_DIR/FsLexYacc.Runtime.dll Absyn.fs FunPar.fs FunLex.fs Parse.fs Fun.fs ParseAndRun.fs
