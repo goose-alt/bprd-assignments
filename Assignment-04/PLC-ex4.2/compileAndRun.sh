1b95d94c789dbf11a318f5e8d198f047a720ad32
Andreas Nicolaj Tietgen <andreotietgen@hotmail.com>
2022-09-27 13:42:02 +0200

    Remove not edited files

--- Assignment-04/PLC-ex4.2/compileAndRun.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-CURRENT_DIR="$( cd "$( dirname "$0" )" && pwd )"
-BIN_DIR=$CURRENT_DIR/../bin
-
-mono $BIN_DIR/fslex.exe --unicode FunLex.fsl \
-    && fsharpi -r $BIN_DIR/FsLexYacc.Runtime.dll Absyn.fs FunPar.fs FunLex.fs Parse.fs Fun.fs ParseAndRun.fs
