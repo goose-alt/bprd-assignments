3a0e6019666234b2a86f50fe54ddab0f1597c77a
Andreas Nicolaj Tietgen <andreotietgen@hotmail.com>
2022-09-29 11:31:51 +0200

    Solved first part of ex 1

--- Assignment-05/PLC-ex5.1/Merge.fsx
+++ Assignment-05/PLC-ex5.1/Merge.fsx
@@ -1,7 +1,15 @@
+module Merge
+
 let merge a b =
 
-    let rec aux acc = function
-        | x :: xs, y :: ys -> 
-            match x, y with
-            | x, y when x =< y -> aux (x :: acc) (x :: xs) ys
-            | 
+    let rec aux acc a b = 
+        match a, b with
+        | [], [] -> acc
+        | x :: xs, [] -> aux (x :: acc) xs []
+        | [], y :: ys -> aux (y :: acc) [] ys
+        | x :: xs, y :: ys when x <= y -> aux (x :: acc) xs (y :: ys)
+        | x :: xs, y :: ys -> aux (y :: acc) (x::xs) ys
+    
+    List.rev (aux [] a b);;
+
+let res = merge [3; 5; 12] [2;3;4;7];;
