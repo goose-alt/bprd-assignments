e2f68bbe3e509d5497d9b2fc6eea2095ae2acc5c
Albert Rise Nielsen <albert@risenielsen.dk>
2022-09-29 12:48:05 +0200

    More compile chain

--- bin/run.sh
+++ bin/run.sh
@@ -2,4 +2,4 @@
 binDir=/data/bin
 utilDir=/data/utils
 
-fsharpi -r $binDir/FsLexYacc.Runtime.dll $@ $utilDir/Parse.fs $utilDir/ParseAndRun.fs
+fsharpi -r $binDir/FsLexYacc.Runtime.dll $@ $utilDir/Parse.fs $utilDir/ParseAndRun.fs $utilDir/ParseAndType.fs
