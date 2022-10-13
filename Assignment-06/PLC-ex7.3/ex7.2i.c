05af9486508a3551c3636d47e506c83bd592f3bf
Andreas Nicolaj Tietgen <andreotietgen@hotmail.com>
2022-10-13 14:16:04 +0200

    Add file from 7.2.i and change while to for loop

---
+++ Assignment-06/PLC-ex7.3/ex7.2i.c
@@ -0,0 +1,26 @@
+//i
+void main(){
+    int arr[4]; 
+    arr[0] = 7;
+    arr[1] = 13;
+    arr[2] = 9;
+    arr[3] = 8;
+
+    int *sump;
+    int result;
+    result = arrsum(4, arr, sump);
+    print(result);
+}
+
+void arrsum(int n, int arr[], int *sump){
+    int i;
+    i = 0;
+    int sum;
+    sum = 0;
+    for (int i = 0; i < n; i = i + 1) {
+        sum = sum + arr[i];
+    }
+
+    sump = sum;
+}
+
